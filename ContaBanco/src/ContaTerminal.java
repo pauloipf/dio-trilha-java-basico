800e72bb1c493a70d79009d7dd13e2b7a83d3bb4
pauloipf <pauloipf@gmail.com>
2023-07-02 00:10:18 -0300

    Projeto final de Conta Bancária da DIO Trilha Java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,38 @@
+
+//Conhecer e importar a class Scanner.
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Olá novo usuário do banco!");
+        // Conhecer e importar a classe Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Exibir as mensagens para nosso usuário.
+        System.out.println("Olá novo usuário do nosso banco! \n" +
+                "Por favor, informe os dados a seguir");
+
+        // Obter pela scanner os valores digitados no terminal.
+        System.out.print("Digite informe número a agência: ");
+        String numeroAgencia = sc.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = sc.nextInt();
+
+        System.out.print("Digite informe seu nome: ");
+        String nomeTitular = String.valueOf(sc.next());
+
+        System.out.print("Digite seu saldo: ");
+        float saldoConta = Float.valueOf(sc.next());
+
+        // Exibir as mensagens de conta criada.
+        System.out.print(nomeTitular + ",\n" +
+                "Obrigado por criar uma conta em nosso banco.\n" +
+                "Sua agência é: " + numeroAgencia + " e conta é: " + numeroConta + "\n" +
+                "Seu saldo de ");
+        System.out.printf("%.2f",saldoConta);
+        System.out.println(" já está disponível para saque.");
+
     }
-
-
 }
